b893de1597d5f8344da1684d583f1af9440be9f0
Carlos Erick <ing.carloserick87@gmail.com>
2025-11-19 00:23:15 +0000

    feat: add new feature for project 2

---
+++ Act_2/Project_2/src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+  constructor() {
+    this.scrollTitle();
+  }
+
+  scrollTitle() {
+    let msg = " Libreria Nexus  -  Cafeteria |  ";
+    let pos = 0;
+
+    const loop = () => {
+      document.title = msg.substring(pos) + msg.substring(0, pos);
+      pos = (pos + 1) % msg.length;
+      setTimeout(loop, 300);
+    };
+
+    loop();
+  }
+}
