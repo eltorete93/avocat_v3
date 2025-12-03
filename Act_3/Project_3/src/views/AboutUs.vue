00cfd558c48f8569f466cdd1bbda630434902a55
Carlos Erick <ing.carloserick87@gmail.com>
2025-12-03 16:11:07 +0000

    chore: update dependencies and fix minor issues

--- Act_3/Project_3/src/views/AboutUs.vue
+++ Act_3/Project_3/src/views/AboutUs.vue
@@ -1,39 +1,27 @@
+<!-- AboutUs -->
+
 <template>
-  <section
-    ref="sectionRef"
-    :class="[
-      'bg-[#f4f4f4] py-16 px-6 transition-all duration-700 ease-out',
-      isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
-    ]"
-  >
+  <section ref="sectionRef" :class="['bg-[#f4f4f4] py-16 px-6 transition-all duration-700 ease-out', isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10']">
     <div class="max-w-7xl mx-auto">
 
-      <!-- Título -->
+      <!-- Títtle -->
       <div class="text-center mb-12">
-        <h2 class="text-4xl font-bold text-[#2c414f]">Nexus</h2>
-        <p class="text-lg mt-3 text-gray-600">
-          Un espacio donde los libros, la creatividad y el café se encuentran
-        </p>
+        <h2 class="text-4xl font-bold text-[#2c414f]">{{ aboutUsData.title }}</h2>
+        <p class="text-lg mt-3 text-gray-600">{{ aboutUsData.subtititle }}</p>
       </div>
 
       <div class="grid md:grid-cols-2 gap-12 items-center">
 
         <!-- Imagen -->
-        <div
-          class="rounded-xl overflow-hidden shadow-lg transform transition hover:-translate-y-1"
-        >
-          <img
-            src="../assets/ChatGPT Image Nov 30, 2025, 09_40_28 PM.png"
-            alt="Nexus"
-            class="w-full h-96 object-cover"
-          />
+        <div class="rounded-xl overflow-hidden shadow-lg transform transition hover:-translate-y-1">
+          <img src="../assets/ChatGPT Image Nov 30, 2025, 09_40_28 PM.png" alt="Nexus" class="w-full h-96 object-cover"/>
         </div>
 
-        <!-- Texto -->
+        <!-- Text -->
         <div class="space-y-6 text-gray-700 leading-relaxed">
 
           <p>
-            En <span class="font-semibold text-[#2c414f]">Nexus</span> creemos que cada historia,
+            En <span class="font-semibold text-[#2c414f]">{{ aboutUsData.title }}</span> creemos que cada historia,
             cada idea y cada taza de café puede conectar personas. Creamos un
             espacio donde lectores, estudiantes y emprendedores pueden crecer.
           </p>
@@ -47,25 +35,25 @@
             para acompañar tus momentos favoritos.
           </p>
 
-          <!-- Iconos -->
+          <!-- Icons -->
           <div class="grid grid-cols-1 sm:grid-cols-3 gap-6 text-center pt-4">
 
             <div class="bg-white p-5 rounded-xl shadow-md hover:shadow-lg transition">
               <BookOpenIcon class="mx-auto w-10 h-10 text-[#2c414f]" />
-              <h4 class="font-semibold mt-3">Librería</h4>
-              <p class="text-sm text-gray-500">Títulos para todos los gustos</p>
+              <h4 class="font-semibold mt-3">{{ aboutUsData.books }}</h4>
+              <p class="text-sm text-gray-500">{{ aboutUsData.subtittleBooks }}</p>
             </div>
 
             <div class="bg-white p-5 rounded-xl shadow-md hover:shadow-lg transition">
               <ComputerDesktopIcon class="mx-auto w-10 h-10 text-[#2c414f]" />
-              <h4 class="font-semibold mt-3">Coworking</h4>
-              <p class="text-sm text-gray-500">Un espacio para crear y trabajar</p>
+              <h4 class="font-semibold mt-3">{{ aboutUsData.coworking }}</h4>
+              <p class="text-sm text-gray-500">{{ aboutUsData.subtititleCoworking }}</p>
             </div>
 
             <div class="bg-white p-5 rounded-xl shadow-md hover:shadow-lg transition">
               <BeakerIcon class="mx-auto w-10 h-10 text-[#2c414f]" />
-              <h4 class="font-semibold mt-3">Cafetería</h4>
-              <p class="text-sm text-gray-500">Café fresco y ambiente acogedor</p>
+              <h4 class="font-semibold mt-3">{{ aboutUsData.cafeteria }}</h4>
+              <p class="text-sm text-gray-500">{{ aboutUsData.subtititleCafeteria }}</p>
             </div>
 
           </div>
@@ -77,6 +65,19 @@
 </template>
 
 <script setup>
+
+const aboutUsData ={
+  title: "Nexus",
+  subtititle:"Un espacio donde los libros, la creatividad y el café se encuentran",
+  books:"Librería",
+  subtittleBooks:"Títulos para todos los gustos",
+  coworking:"Coworking",
+  subtititleCoworking:"Espacio moderno y colaborativo",
+  cafeteria:"Cafetería",
+  subtititleCafeteria:"Café fresco y ambiente acogedor"
+
+}
+
 import { ref, onMounted } from "vue";
 import {
   BookOpenIcon,
